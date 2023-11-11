df9c9d8768e2bc8407b270e1e291932f8d316048
elixsteelers <143569020+elixsteelers@users.noreply.github.com>
2023-11-10 23:50:51 -0600

    Add files via upload

---
+++ EJERCICIO_8/src/ejercicio_8/EJERCICIO_8.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio_8;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author juani
+ */
+public class EJERCICIO_8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char Caracter;
+        Scanner input = new Scanner(System.in);
+        do {
+            System.out.println("Introduce un carácter (o espacio en blanco para salir): ");
+            Caracter = input.nextLine().charAt(0);
+            if (Caracter != ' ') {
+                if (Caracter == 'a' || Caracter == 'e' || Caracter == 'i' || Caracter == 'o' || Caracter == 'u' ||
+                    Caracter == 'A' || Caracter == 'E' || Caracter == 'I' || Caracter == 'O' || Caracter == 'U') {
+                    System.out.println(Caracter + " es una vocal.");
+                } else {
+                    System.out.println(Caracter + " es una consonante.");
+                }
+            }
+        } while (Caracter != ' ');
+        System.out.println("Programa terminado.");
+    }
+    }
---
+++ EJERCICIO_9/src/ejercicio_9/EJERCICIO_9.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio_9;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author juani
+ */
+public class EJERCICIO_9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingrese una cadena de texto: ");
+        String letra =input.nextLine();
+        System.out.println("Ingrese el caracter a buscar: ");
+        char text =input.nextLine().charAt(0);
+        int count = 0;
+        for (int i = 0; i < letra.length(); i++) {
+            if (letra.charAt(i) == text) {
+                count++;
+            }
+        }
+        System.out.println("El caracter " + text + " aparece " + count + " veces en la cadena.");
+    }
+}
